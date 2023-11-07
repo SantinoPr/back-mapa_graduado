2af8ff4aad89c57c8bd92c76cab7710f3b134bca
Santino <santino-albarello@hotmail.com>
2023-11-07 17:48:07 -0300

    Agregar archivos de proyecto.

---
+++ back mapa_graduado/Controllers/GraduadoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace back_mapa_graduado.Controllers
+{
+    [ApiController]
+    [Route("graduado")]
+    public class GraduadoController : ControllerBase
+    {
+        SortedList<string,Graduado > graduados = new SortedList<string,Graduado>();
+
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic GuardarGraduado([FromBody]Graduado graduado)
+        {
+            graduados.Add(graduado.Cuil,graduado);
+            Console.WriteLine("Datos recibido con exito");
+            return new
+            {
+                succes = true,
+                message = "Graduado guardado con exito",
+            };
+
+        }
+
+        [HttpGet]
+        [Route("listar")]
+        public dynamic ListarGraduado(string cuil)
+        {
+           if(graduados.ContainsKey(cuil))
+                return graduados[cuil];
+            return new
+            {
+                succes = true,
+                message = "Graduado no encontrado"
+            };
+        }
+    }
+
+   }
+
---
+++ back mapa_graduado/Models/Graduado.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace back_mapa_graduado.Controllers
+{
+    public class Graduado
+    {
+        private string cuil;
+        private string nombre;
+        private string mail;
+        private string numero;
+        private string carrera;
+        private string ciudad;
+        private string latitud;
+        private string longitud;
+        //private Bitmap foto;
+
+        public string Cuil { get => cuil; set => cuil = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public string Carrera { get => carrera; set => carrera = value; }
+        public string Ciudad { get => ciudad; set => ciudad = value; }
+        public string Latitud { get => latitud; set => latitud = value; }
+        public string Longitud { get => longitud; set => longitud = value; }
+        //public Bitmap Foto { get => foto; set => foto = value; }
+
+    }
+}
